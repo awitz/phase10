07705a7f94ab2ac13470463553faf925b715f291
Amanda Witz <amandawitz@Amandas-MacBook-Air.local>
2017-05-07 19:52:03 -0700

    Sort function updated & working. Discard & Draw functions working. Can't laydown phase yet

--- phase10.py
+++ phase10.py
@@ -7,7 +7,6 @@ def create_deck ():
 
     # make a deck of cards
     deck = list(itertools.product(range(1,12),['Red','Red','Blue','Blue','Yellow','Yellow','Green','Green']))
-
     # shuffle the cards
     random.shuffle(deck)
     return deck
@@ -25,7 +24,7 @@ def deal_cards (player1_hand,player2_hand,deck,discard):
 def deal_player_hands(deck):
     player_hand = []
     for i in range(10):
-        player_hand.append([deck[i][0], deck[i][1]])  
+        player_hand.append(deck[i])
     del deck [0:10]
     return player_hand
 
@@ -34,28 +33,34 @@ def is_winner(player_hand):
 
 def display_players_hand_discard(player_hand,player,discard):
     sort_player_hand(player_hand)
-    print "\n Player " + str(player) + " hand: " + str(player_hand)
-    print "\n Discard pile: {}".format(discard)
+    print "\nPlayer " + str(player) + " hand: "
+    
+    i=0
+    for card in player_hand:
+        print str(i) + " : {}".format(card)
+        i += 1
+    print "\nDiscard pile: " + "\n" + str(discard)
 
 def sort_player_hand(player_hand):
-    return player_hand.sort()
+    player_hand.sort()
 
 def draw_a_card(deck,player_hand,player):
     player_hand.append(deck[0])
+    print "You picked up a {}".format(deck[0])
     del deck[0]
     sort_player_hand(player_hand)
     display_players_hand_discard(player_hand,player,discard)
     
-def decide_to_laydown_or_discard(player_hand,discard):
+def decide_to_laydown_or_discard(player_hand,discard,player):
     input = True
     while input == True:
-        user_input = raw_input("\n Would you like to lay down phase (L) or Discard (D)? ").upper()
+        user_input = raw_input("\nWould you like to lay down phase (L) or Discard (D)? ").upper()
         if user_input == "L":
             laydown_phase(player_hand)
-            discard_from_hand(player_hand,discard)
+            discard_from_hand(player_hand,discard,player)
             input = False
         elif user_input == "D":
-            discard_from_hand(player_hand,discard)
+            discard_from_hand(player_hand,discard,player)
             input = False
         else:
             print "incorrect entry"
@@ -66,9 +71,13 @@ def pick_up_discard(player_hand,discard,player):
     sort_player_hand(player_hand)
     display_players_hand_discard(player_hand,player,discard)
 
-def discard_from_hand(player_hand,discard):
+def discard_from_hand(player_hand,discard,player):
     user_input = int(raw_input("Which card index would you like to discard? "))
-    discard[0] = player_hand[user_input]
+    display_players_hand_discard(player_hand,player,discard)
+    if len(discard) == 0:
+        discard.append(player_hand[user_input])
+    else:
+        discard[0] = player_hand[user_input]
     del player_hand[user_input]
 
 def laydown_phase(player_hand):
@@ -83,12 +92,12 @@ def players_turn(player_hand,discard,deck,player):
     elif user_input == "P":
         pick_up_discard(player_hand,discard,player)
 
-    decide_to_laydown_or_discard(player_hand,discard)
+    decide_to_laydown_or_discard(player_hand,discard,player)
 
 def run_game(player1_hand,player2_hand,deck,discard):
     player = 1
     while True:
-        print "\n It is Player {}s turn.".format(player)
+        print "\nIt is Player {}s turn.".format(player)
         if player == 1:
             players_turn(player1_hand,discard,deck,player)
             player = 2
